15803fe6307ac4a7634558b41f461350fef629d7
MustafaMahmoud <mustafamabdelaziz@gmail.com>
2020-12-25 14:49:51 +0200

    Update calculator_mips.asm

--- calculator_mips.asm
+++ calculator_mips.asm
@@ -11,8 +11,6 @@ ch8: .asciiz "8-Square detection  \n"
 ch9: .asciiz "9-Reactangle detection  \n"
 ch10: .asciiz "10-Triangle detection  \n"
 st1: .asciiz "Enter 2 numbers \n"
-st2: .asciiz "Enter 4 length of each side \n"
-st3: .asciiz "Enter 3 length of each side \n"
 an1: .asciiz "\nAddition is \n"
 an2: .asciiz "\nmin of list is \n"
 an3: .asciiz "\nMultiple is \n"
@@ -21,17 +19,19 @@ degree: .asciiz "\enter the degree \n"
 Opposite:.asciiz "\enter the opposite to the angle ?\n"
 Adjacent:.asciiz "\enter  the adjacent (next to) to the angle ?\n"
 Hypotenuse:.asciiz "\enter  the adjacent (next to) to the angle ?\n"
-an5: .asciiz "\nSin is: \t"
-an6: .asciiz "\nCos is: \t"
-an7: .asciiz "\nTan is: \t"
-an8: .asciiz "\n Square detection is: \t"
-an9: .asciiz "\n Rectangle detection is: \t"
-an10: .asciiz "\n Triangle detection is: \t"
+an6: .asciiz "\nCos is \n"
+an7: .asciiz "\nTan is \n"
+an8: .asciiz "\n Square detection is \n"
+an9: .asciiz "\n Rectangle detection is   \n"
+an10: .asciiz "\n Triangle detection is \n"
 Dividend:     .asciiz "\nPlease Enter the Dividend: "
 Divisor:      .asciiz "\nPlease Enter the Divisor: "
 Result:       .asciiz "\nThe Result is: "
 Remainder:    .asciiz "\nthe Remainderis: "
 ReadasAfloat:   .float 0.0
+MinMsg1: .asciiz"Enter the array elemnts: "
+MinMsg2: .asciiz"Enter the array size: "
+array: .word 80
 #$s0 contains number 1
 #$sl contains number 2
 #$s2 contains number 
@@ -140,23 +140,55 @@ j exit
 
 #Case 2 Will  execute this part
 #Mostafa Section Minmuim of list
+
 min: 
-#Enter size of array numbers 
+
+#Enter the size of the array
+li $v0,4
+la $a0,MinMsg2
+syscall
+li $v0, 5
+syscall
+move $t3, $v0
+addi $t2, $zero,4
+mul $t3, $t3, $t2
 
 #Enter numbers of array
+li $v0,4
+la $a0,MinMsg1
+syscall
 
+addi $t0, $zero,0
+while:
+	beq $t0,$t3,while1 
+	li $v0,5
+	syscall
+	move $s0,$v0
+	sw $s0, array($t0)
+	addi $t0,$t0,4
 
+	j while
 #start to find minmuim
-######## remove this and start Your fuckn work#####
-
-
-######## remove this and start Your fuckn work#####
-
+addi $t5, $zero,0
+lw $s4,array($t0)
+while1:
+	beq $t5,$t3,printresult 
+	lw $s6, array($t5)
+	addi $t5, $t5,4
+	slt $s0,$s6,$s4
+	bne $s0,$zero,assignMin
+	j while1
+	
+	
+assignMin:
+move $s4, $s6
+j while1
+printresult:
 li $v0,4
 la $a0, ch2
 syscall
 li $v0,1
-add $a0, $zero, $s4
+move $a0,$t4
 syscall
 
 
@@ -167,27 +199,25 @@ j exit
 # ; section multiplication
 mull:
 #Enter Two numbers
-li $v0,4
-la $a0, st1
-syscall
-li $v0,5
-syscall
-addi $s0,$v0,0
-li $v0,5
-syscall
-addi $s1,$v0,0
-li $v0,4
-syscall
-#########
-li $v0,4
-la $a0,an3
-syscall
-mult $s0,$s1
-li $v0,1
-mfhi $a0
-syscall
-mflo $a0
-syscall
+     li $v0, 4  #command for printing a string
+     la $a0, st1  #loading the string into st1 enable printing
+     syscall
+     li $v0, 5 #command for reading an integer
+     syscall
+     add $t0, $v0, $zero
+     li $v0, 5 #command for reading an integer
+     syscall
+     add $t1, $v0, $zero 
+     mult $t0, $t1  #this adds the values stored in t1 and assigns them to the temporary register $t0
+     li $v0, 4 #command for printing a string
+     la $a0, Result  #loading the string into Result enable printing
+     syscall
+     li $v0, 1
+     mfhi $a0 # Move From High  register
+     syscall
+     li $v0, 1
+     mflo $a0  # Move From Low register
+     syscall
 j exit
 
 #Case 4 Will  execute this part
@@ -207,16 +237,16 @@ divv:
      li $v0, 5 #command for reading an integer
      syscall  #executing the command
      add $t1, $v0, $zero
-         mul $t6,$t0,$t1 #this adds the values stored in $t0 and $t1 and assigns them to the temporary register $t6
+     mul $t6,$t0,$t1 #this adds the values stored in $t0 and $t1 and assigns them to the temporary register $t6
 
 #start division
-     div $t0, $t1 #this adds the values stored in $t1 and assigns them to the temporary register $t1
+     div $t0, $t1 #this adds the values stored in $t1 and assigns them to the temporary register $t0
      li $v0, 4  #this is the command for printing a string
      la $a0, Result #loading the string into Result enable printing
      syscall
 
      li $v0, 1 
-     mflo $a0
+     mflo $a0  # Move From Low register
      syscall
 
      li $v0, 4 #this is the command for printing a string
@@ -224,7 +254,7 @@ divv:
      syscall
      
      li $v0, 1
-     mfhi $a0
+     mfhi $a0  # Move From High register
      syscall
 j exit
 
@@ -252,22 +282,11 @@ li $v0, 4   #command for printing a string
      syscall    #executing the command
      add.s $f6, $f0, $f4
      
+<<<<<<< Updated upstream
 
+=======
+>>>>>>> Stashed changes
 
-    li $v0, 4   #command for printing a string
-    la $a0, Opposite #loading the string into degree enable printing
-    syscall      #executing the command
-     li $v0, 6      #command for reading an integer
-     syscall    #executing the command
-     add.s $f5, $f0, $f4
-     
-    li $v0, 4   #command for printing a string
-    la $a0, Hypotenuse #loading the string into degree enable printing
-    syscall      #executing the command
-     li $v0, 6      #command for reading an integer
-     syscall    #executing the command
-     add.s $f6, $f0, $f4
-     
 
 #start sin
      div.s $f12,$f5, $f6  #this adds the values stored in $t1 and assigns them to the temporary register $t0
@@ -370,7 +389,7 @@ square:
 #Enter  numbers
 
 li $v0,4
-la $a0, st2
+la $a0, st1
 syscall
 li $v0,5
 syscall
@@ -387,6 +406,7 @@ addi $s7,$v0,0
 li $v0,4
 syscall
 ########
+
 #start to find square
 beq  $s4,$s5,Sq1
 li $t4,0
@@ -403,8 +423,8 @@ Sq3:
 li $t4,1
 ExitSq:
 #####
-li $v0, 4
-la $a0, an8
+li $v0,4
+la $a0, ch8
 syscall
 add $a0, $t4, $0
 li $v0, 1
@@ -415,8 +435,10 @@ j exit
 rectangle:
 
 #Enter numbers
+
+
 li $v0,4
-la $a0, st2
+la $a0, st1
 syscall
 li $v0,5
 syscall
@@ -458,8 +480,9 @@ Rect33:
 li $t4,1
 ExitRect:
 ########
-li $v0, 4
-la $a0, an9
+
+li $v0,4
+la $a0, ch9
 syscall
 add $a0, $t4, $0
 li $v0, 1
@@ -470,7 +493,7 @@ j exit
 triangle:
 #Enter three numbers
 li $v0,4
-la $a0, st3
+la $a0, st1
 syscall
 li $v0,5
 syscall
@@ -483,6 +506,11 @@ syscall
 addi $s6,$v0,0
 li $v0,4
 syscall
+<<<<<<< Updated upstream
+=======
+
+
+>>>>>>> Stashed changes
 
 ####Triangle detection
 
@@ -511,6 +539,10 @@ ExitTri:
 ######
 li $v0,4
 la $a0,an10
+syscall
+
+li $v0,4
+la $a0, ch10
 syscall
 add $a0, $s7, $0
 li $v0, 1
